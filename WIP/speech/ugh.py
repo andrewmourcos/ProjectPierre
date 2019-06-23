e44df6b2277602ed8ccd38087f22354baf483235
roberttoyonaga <robert.toyonaga@gmail.com>
2019-06-23 01:31:32 -0400

    db additions, updates, users, clearing user data

--- WIP/speech/ugh.py
+++ WIP/speech/ugh.py
@@ -2,9 +2,19 @@
 import os
 from pocketsphinx import LiveSpeech, get_model_path
 import requests
+name = str(input('what is your username'))
+command = str(input('what should I do? [record] or [clear]'))
+if command=='clear':
+    req = requests.get('https://roberttoyonaga.api.stdlib.com/pierre-sheets@dev/?operation=entries')
+    req_list = req.content.decode('utf-8').split("\"")
+    for i in req_list:
+        if name in i:
+            requests.get('https://roberttoyonaga.api.stdlib.com/pierre-sheets@dev/?operation=clear&myKey='+i)
 
-precede_list = ['and','and(2)','you','or', 'an(2)']
+    exit()
 
+precede_list = ['and','and(2)','you','or', 'an(2)','ok', 'okay']
+single_list = ['just','just(2)','normally']
 def parse(sentence):#,fillers):
     iSaid = []
     last_word = ''
@@ -20,9 +30,17 @@ def parse(sentence):#,fillers):
         if word=='so' and (last_word in precede_list):
                 iSaid.append(word)
         if (word=='no' or word=='know') and (last_word in precede_list):
-            iSaid.append(word)
+            iSaid.append('you know')
         if word=='something' and (last_word in precede_list):
             iSaid.append(word)
+        if word in single_list:
+                iSaid.append(word)
+        if word=='also' and (last_word in precede_list):
+                iSaid.append(word)
+        if word=='yeah' and (last_word in precede_list):
+                iSaid.append(word)
+        if word=='in' and last_word=='as':
+                iSaid.append('as in')
         last_word=word
     return iSaid, sentence
 
@@ -40,14 +58,31 @@ speech = LiveSpeech(
     lm=os.path.join(model_path, 'en-us.lm.bin'),
     dic=os.path.join(model_path, 'cmudict-en-us.dict')
 )
-
-#fillers= ['and','essentially', 'okay', 'basically', 'so', 'like']
+filler_words = []
+frequencies = []
 
 for phrase in speech:
     filler, sentence = parse(phrase.segments())
-    joined_filler = '.'.join(filler)
+    joined_filler = ','.join(filler)
 
     print(joined_filler,sentence)
     if len(filler)!=0:
         print(requests.get('https://roberttoyonaga.api.stdlib.com/pierre-sms@dev/?name='+joined_filler))
+        for filler_word in filler:
+            if filler_word not in filler_words:
+                filler_words.append(filler_word)
+                frequencies.append(1)
+            else:
+                frequencies[filler_words.index(filler_word)]+=1
+
+            for filler_word in filler_words:
+                requests.get('https://roberttoyonaga.api.stdlib.com/pierre-sheets@dev/?operation=set&data='+str(frequencies[filler_words.index(filler_word)])+
+                '&myKey='+name+'_'+filler_word)
+
+
+print(filler_words)
+print(frequencies)
+
+
+
 
